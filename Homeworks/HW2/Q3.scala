1339b7d639bfa8c81ad9767a8776a4be5ea3df93
NishkaDG <nishka.dasgupta@yahoo.com>
2016-09-10 19:00:16 +0530

    Update Q3.scala

--- Homeworks/HW2/Q3.scala
+++ Homeworks/HW2/Q3.scala
@@ -2,12 +2,12 @@ object Q3{
 	def main(args: Array[String]): Unit=
 	{
 	}	
-	def sortInsert(x: Int, l: List[Int]): List[Int]=
+	def sortInsert(l: List[Int], x: Int): List[Int]=
 	{
 		l match{
 			case Nil => List(x)
 			case  hd::rest if x<=hd => x::l
-			case hd::rest if x>hd => hd::sortInsert(x, rest)
+			case hd::rest if x>hd => hd::sortInsert(rest, x)
 			}
 		}
 }
