33e019d903decc2d6ddf5d77a39ffe80207e66b2
NishkaDG <nishka.dasgupta@yahoo.com>
2016-09-10 19:33:29 +0530

    Update Q2.scala

--- Homeworks/HW2/Q2.scala
+++ Homeworks/HW2/Q2.scala
@@ -1,14 +1,14 @@
 object Q2{
-	def main(args: Array[String]): Unit=
+	def main(args: Array[String]): Unit= //main function
 	{
 	
 	}
 	def Split(l: List[Int]): (List[Int], List[Int])=
 	{	
 		l match{
-			case Nil => (Nil, Nil)
+			case Nil => (Nil, Nil) //base case for even length list
 			case hd::rest if l.length>1 => (hd::Split(rest.init)._1, Split(rest.init)._2:::List(l.last))
-			case hd::rest if l.length==1 => (List(hd), Nil)
+			case hd::rest if l.length==1 => (List(hd), Nil) //base case for odd length list
 		}
 		}
 }
