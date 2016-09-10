48b3a9fa93cf31719cb8735f153632b5b2197046
NishkaDG <nishka.dasgupta@yahoo.com>
2016-09-10 19:53:23 +0530

    Update Q7.scala

--- Homeworks/HW2/Q7.scala
+++ Homeworks/HW2/Q7.scala
@@ -1,13 +1,13 @@
 object Q7{
-	def main(args: Array[String]): Unit=
+	def main(args: Array[String]): Unit=//main function
 	{
 	
 	}
-	def fastPower(x: Int, n: Int): Int=
+	def fastPower(x: Int, n: Int): Int= //will work when n is a power of 2
 	{
 		n match{
-			case 2 => x*x
-			case _ => fastPower(x,n/2)*fastPower(x,n/2)
+			case 2 => x*x //base case
+			case _ => fastPower(x,n/2)*fastPower(x,n/2) //to build up x^n from x^2
 		}
 	}
 }
