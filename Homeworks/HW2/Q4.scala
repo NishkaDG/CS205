c7e3e86d7e316ba87fc813b362b9283592b66089
NishkaDG <nishka.dasgupta@yahoo.com>
2016-09-10 19:51:39 +0530

    Update Q4.scala

--- Homeworks/HW2/Q4.scala
+++ Homeworks/HW2/Q4.scala
@@ -1,21 +1,24 @@
 object Q4{
-	def main(args: Array[String]): Unit=
+	def main(args: Array[String]): Unit= //main method
 	{
 	
 	}
-	def sortInsert(x: Int, l: List[Int]): List[Int]=
+	def sortInsert(l: List[Int], x: Int): List[Int]= //to insert each element in its rightful place
 	{
 		l match{
 			case Nil => List(x)
 			case  hd::rest if x<=hd => x::l
-			case hd::rest if x>hd => hd::sortInsert(x, rest)
+			case hd::rest if x>hd => hd::sortInsert(rest, x)
 			}
 	}	
 	def InsertionSort(lst: List[Int]): List[Int]=
 	{
 		lst match{
-			case hd::rest => sortInsert(hd, InsertionSort(rest))
-			case _ => lst
+			/**This will sort the last n-1 elements of the list, where n is the length of the list.
+			  *Then it will insert the first element of the list in its rightful place using sortInsert
+			  */
+			case hd::rest => sortInsert(InsertionSort(rest), hd)
+			case _ => lst //base case
 		}
 	}
 }
